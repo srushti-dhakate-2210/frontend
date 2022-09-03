61cbb2295626167a190c3e1aab0ca4e2514379c9
smritiagrawall <78158137+smritiagrawall@users.noreply.github.com>
2022-09-03 16:45:53 +0530

    style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 function App() {
+const head = {
+  color: "#9441DE",
+  fontSize: 45,
+  paddingTop:100,
+  paddingLeft:80,
+  
+}
+
+const para ={
+  fontSize: 20,
+  color:"#FBFAFC",
+  paddingLeft:80,
+ 
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 style={head}>O(1) Coding Club</h1>
+     <p style={para}>Closer to your AMBITIONS,<br />
+     a step at a time.</p>
     </div>
   );
 }
