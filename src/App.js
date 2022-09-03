cf45bb7930b08b9bd41e3582c00d4cae95147065
smritiagrawall <78158137+smritiagrawall@users.noreply.github.com>
2022-09-03 20:55:38 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import { Parallax } from 'react-parallax';
+import nebula from './image/nebula.jpeg';
 function App() {
-const head = {
-  color: "#9441DE",
-  fontSize: 45,
-  paddingTop:100,
-  paddingLeft:80,
+  const head = {
+    color: "#9441DE",
+    fontSize: 70,
+    paddingTop:100,
+    paddingLeft:80,
+    
+  }
   
-}
-
-const para ={
-  fontSize: 20,
-  color:"#FBFAFC",
-  paddingLeft:80,
- 
-}
-
+  const para ={
+    fontSize: 30,
+    color:"#FBFAFC",
+    paddingLeft:80,
+   
+  }
   return (
     <div className="App">
+      <home />
+     <Parallax strength={600} bgImage={nebula}>
+     <div className="content">
+     <div className="text-content">
      <h1 style={head}>O(1) Coding Club</h1>
-     <p style={para}>Closer to your AMBITIONS,<br />
-     a step at a time.</p>
+    <p style={para}>Closer to your AMBITIONS,<br />
+    a step at a time.</p>
+     </div>
+     </div>
+     </Parallax>
+     <div className="content"></div>
+   
     </div>
   );
 }
